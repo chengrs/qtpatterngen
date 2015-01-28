7f290421ce1a09b58fcf8004602f719e199fc7ea
Ray Cheng <chengrs@gmail.com>
2015-01-28 10:17:21 +0800

    refine V-Half pattern

--- PatternVHalf.cpp
+++ PatternVHalf.cpp
@@ -2,7 +2,6 @@
 
 PatternVHalf::PatternVHalf(int m_height)
 {
-//    qDebug() << "PatternVHalf()";
     // init values
     m_fgColor = Colors::K;
     m_bgColor = Colors::W;
@@ -20,8 +19,6 @@ PatternVHalf::~PatternVHalf()
 
 void PatternVHalf::drawPattern(QPainter &painter, Pattern::PaintingLevel &ground, Colors::Color &color, int grayLevel)
 {
-//    qDebug() << "drawPattern()";
-
     if (ground == Pattern::ForeGround) {
         qDebug() << "Pattern::ForeGround";
         m_fgColor = color;
@@ -38,73 +35,67 @@ void PatternVHalf::drawPattern(QPainter &painter, Pattern::PaintingLevel &ground
     painter.setRenderHint(QPainter::Antialiasing, false);
     painter.setPen(Qt::NoPen);
 
-    QBrush *brush = NULL;
+    QBrush brush(Qt::black, Qt::SolidPattern);
 
     switch (m_bgColor) {
     case(Colors::K):
-        brush = new QBrush(QColor(0, 0, 0, 255));
+        brush.setColor(QColor(0, 0, 0, 255));
         break;
     case(Colors::R):
-        brush = new QBrush(QColor(m_bgGrayLevel, 0, 0, 255));
+        brush.setColor(QColor(m_bgGrayLevel, 0, 0, 255));
         break;
     case(Colors::G):
-        brush = new QBrush(QColor(0, m_bgGrayLevel, 0, 255));
+        brush.setColor(QColor(0, m_bgGrayLevel, 0, 255));
         break;
     case(Colors::Y):
-        brush = new QBrush(QColor(m_bgGrayLevel, m_bgGrayLevel, 0, 255));
+        brush.setColor(QColor(m_bgGrayLevel, m_bgGrayLevel, 0, 255));
         break;
     case(Colors::B):
-        brush = new QBrush(QColor(0, 0, m_bgGrayLevel, 255));
+        brush.setColor(QColor(0, 0, m_bgGrayLevel, 255));
         break;
     case(Colors::M):
-        brush = new QBrush(QColor(m_bgGrayLevel, 0, m_bgGrayLevel, 255));
+        brush.setColor(QColor(m_bgGrayLevel, 0, m_bgGrayLevel, 255));
         break;
     case(Colors::A):
-        brush = new QBrush(QColor(0, m_bgGrayLevel, m_bgGrayLevel, 255));
+        brush.setColor(QColor(0, m_bgGrayLevel, m_bgGrayLevel, 255));
         break;
     case(Colors::W):
     default:
-        brush = new QBrush(QColor(m_bgGrayLevel, m_bgGrayLevel, m_bgGrayLevel, 255));
+        brush.setColor(QColor(m_bgGrayLevel, m_bgGrayLevel, m_bgGrayLevel, 255));
         break;
     }
 
-    painter.setBrush(*brush);
+    painter.setBrush(brush);
     painter.drawRect(rectLower);
-
-    delete brush;
-
-//    brush = new QBrush(QColor(m_fgGrayLevel, 0, 0, 255));
 
     switch (m_fgColor) {
     case(Colors::K):
-        brush = new QBrush(QColor(0, 0, 0, 255));
+        brush.setColor(QColor(0, 0, 0, 255));
         break;
     case(Colors::R):
-        brush = new QBrush(QColor(m_fgGrayLevel, 0, 0, 255));
+        brush.setColor(QColor(m_fgGrayLevel, 0, 0, 255));
         break;
     case(Colors::G):
-        brush = new QBrush(QColor(0, m_fgGrayLevel, 0, 255));
+        brush.setColor(QColor(0, m_fgGrayLevel, 0, 255));
         break;
     case(Colors::Y):
-        brush = new QBrush(QColor(m_fgGrayLevel, m_fgGrayLevel, 0, 255));
+        brush.setColor(QColor(m_fgGrayLevel, m_fgGrayLevel, 0, 255));
         break;
     case(Colors::B):
-        brush = new QBrush(QColor(0, 0, m_fgGrayLevel, 255));
+        brush.setColor(QColor(0, 0, m_fgGrayLevel, 255));
         break;
     case(Colors::M):
-        brush = new QBrush(QColor(m_fgGrayLevel, 0, m_fgGrayLevel, 255));
+        brush.setColor(QColor(m_fgGrayLevel, 0, m_fgGrayLevel, 255));
         break;
     case(Colors::A):
-        brush = new QBrush(QColor(0, m_fgGrayLevel, m_fgGrayLevel, 255));
+        brush.setColor(QColor(0, m_fgGrayLevel, m_fgGrayLevel, 255));
         break;
     case(Colors::W):
     default:
-        brush = new QBrush(QColor(m_fgGrayLevel, m_fgGrayLevel, m_fgGrayLevel, 255));
+        brush.setColor(QColor(m_fgGrayLevel, m_fgGrayLevel, m_fgGrayLevel, 255));
         break;
     }
 
-    painter.setBrush(*brush);
+    painter.setBrush(brush);
     painter.drawRect(rectUpper);
-
-    delete brush;
 }
