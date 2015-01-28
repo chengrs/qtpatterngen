a3fa427d0c07611a7b12a95e0e514a0056259b4b
Ray Cheng <chengrs@gmail.com>
2015-01-28 09:59:56 +0800

    refine Color Bar pattern

--- PatternColorBar.cpp
+++ PatternColorBar.cpp
@@ -18,43 +18,43 @@ void PatternColorBar::drawPattern(QPainter &painter, Pattern::PaintingLevel &, C
 
     QLinearGradient gradient(rect.left(), rect.height() / 2, rect.right(), rect.height() / 2);
 
-    QRect rect1(0, 0, m_width, height);
+    rect.setRect(0, 0, m_width, height);
     gradient.setColorAt(0, QColor(255, 255, 255, 255));
     gradient.setColorAt(1, QColor(0, 0, 0, 255));
-    painter.fillRect(rect1, gradient);
+    painter.fillRect(rect, gradient);
 
-    QRect rect2(0, height, m_width, height);
+    rect.setRect(0, height, m_width, height);
     gradient.setColorAt(0, QColor(0, 0, 0, 255));
     gradient.setColorAt(1, QColor(255, 255, 255, 255));
-    painter.fillRect(rect2, gradient);
+    painter.fillRect(rect, gradient);
 
-    QRect rect3(0, height * 2, m_width, height);
+    rect.setRect(0, height * 2, m_width, height);
     gradient.setColorAt(0, QColor(255, 0, 0, 255));
     gradient.setColorAt(1, QColor(0, 0, 0, 255));
-    painter.fillRect(rect3, gradient);
+    painter.fillRect(rect, gradient);
 
-    QRect rect4(0, height * 3, m_width, height);
+    rect.setRect(0, height * 3, m_width, height);
     gradient.setColorAt(0, QColor(0, 0, 0, 255));
     gradient.setColorAt(1, QColor(255, 0, 0, 255));
-    painter.fillRect(rect4, gradient);
+    painter.fillRect(rect, gradient);
 
-    QRect rect5(0, height * 4, m_width, height);
+    rect.setRect(0, height * 4, m_width, height);
     gradient.setColorAt(0, QColor(0, 255, 0, 255));
     gradient.setColorAt(1, QColor(0, 0, 0, 255));
-    painter.fillRect(rect5, gradient);
+    painter.fillRect(rect, gradient);
 
-    QRect rect6(0, height * 5, m_width, height);
+    rect.setRect(0, height * 5, m_width, height);
     gradient.setColorAt(0, QColor(0, 0, 0, 255));
     gradient.setColorAt(1, QColor(0, 255, 0, 255));
-    painter.fillRect(rect6, gradient);
+    painter.fillRect(rect, gradient);
 
-    QRect rect7(0, height * 6, m_width, height);
+    rect.setRect(0, height * 6, m_width, height);
     gradient.setColorAt(0, QColor(0, 0, 255, 255));
     gradient.setColorAt(1, QColor(0, 0, 0, 255));
-    painter.fillRect(rect7, gradient);
+    painter.fillRect(rect, gradient);
 
-    QRect rect8(0, height * 7, m_width, height);
+    rect.setRect(0, height * 7, m_width, height);
     gradient.setColorAt(0, QColor(0, 0, 0, 255));
     gradient.setColorAt(1, QColor(0, 0, 255, 255));
-    painter.fillRect(rect8, gradient);
+    painter.fillRect(rect, gradient);
 }
