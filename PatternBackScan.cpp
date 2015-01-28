26523c5c396deae2d90a462d63ca0b6d185f6585
Ray Cheng <chengrs@gmail.com>
2015-01-28 09:52:40 +0800

    refine Back Scan pattern

--- PatternBackScan.cpp
+++ PatternBackScan.cpp
@@ -19,63 +19,52 @@ void PatternBackScan::drawPattern(QPainter &painter, Pattern::PaintingLevel &, C
     int height = m_height / 2;
     int width = m_width / 8;
 
-    QBrush *brush = NULL;
+    QBrush brush(Qt::black, Qt::SolidPattern);
+    QRect rect(0, 0, width, height);
 
-    QRect rect1(0, 0, width, height);
-    brush = new QBrush(QColor(0, 0, 0, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect1);
-    delete brush;
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect2(width, 0, width, height);
-    brush = new QBrush(QColor(0, 0, 255, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect2);
-    delete brush;
+    rect.setRect(width, 0, width, height);
+    brush.setColor(Qt::blue);
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect3(width * 2, 0, width, height);
-    brush = new QBrush(QColor(255, 0, 0, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect3);
-    delete brush;
+    rect.setRect(width * 2, 0, width, height);
+    brush.setColor(Qt::red);
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect4(width * 3, 0, width, height);
-    brush = new QBrush(QColor(255, 0, 255, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect4);
-    delete brush;
+    rect.setRect(width * 3, 0, width, height);
+    brush.setColor(Qt::magenta);
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect5(width * 4, 0, width, height);
-    brush = new QBrush(QColor(0, 255, 0, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect5);
-    delete brush;
+    rect.setRect(width * 4, 0, width, height);
+    brush.setColor(Qt::green);
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect6(width * 5, 0, width, height);
-    brush = new QBrush(QColor(0, 255, 255, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect6);
-    delete brush;
+    rect.setRect(width * 5, 0, width, height);
+    brush.setColor(Qt::cyan);
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect7(width * 6, 0, width, height);
-    brush = new QBrush(QColor(255, 255, 0, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect7);
-    delete brush;
+    rect.setRect(width * 6, 0, width, height);
+    brush.setColor(Qt::yellow);
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect8(width * 7, 0, width, height);
-    brush = new QBrush(QColor(255, 255, 255, 255));
-    painter.setBrush(*brush);
-    painter.drawRect(rect8);
-    delete brush;
+    rect.setRect(width * 7, 0, width, height);
+    brush.setColor(Qt::white);
+    painter.setBrush(brush);
+    painter.drawRect(rect);
 
-    QRect rect(0, 0, m_width, height);
+    rect.setRect(0, 0, m_width, height);
     QLinearGradient gradient(rect.left(), rect.height() / 2, rect.right(), rect.height() / 2);
 
-    QRect rect9(0, height, m_width, height);
+    rect.setRect(0, height, m_width, height);
     gradient.setColorAt(0, QColor(0, 0, 0, 255));
     gradient.setColorAt(1, QColor(255, 255, 255, 255));
-    painter.fillRect(rect9, gradient);
-
-    delete brush;
+    painter.fillRect(rect, gradient);
 }
